961100e40d3e12608c9d75c79668d440424b62f5
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-12-06 16:59:41 -0330

    jest changes

--- services/test/beers.dal.test.js
+++ services/test/beers.dal.test.js
@@ -9,25 +9,8 @@ const { getBeers } = require("../beers.dal");
 describe("beers table get data", () => {
   let rowCount = 0;
   beforeAll(async () => {
-    const result = await dal.query("select count(*) from beers2");
+    const result = await dal.query("select count(*) from beers");
     rowCount = parseInt(result.rows[0].count);
-    const testBeer = {
-      name: "test",
-      abv: 1,
-      ibu: 1,
-      category_id: 1,
-      brewery_id: 1,
-    };
-    await dal.query(
-      "insert into beers2(name, abv, ibu, category_id, brewery_id) values($1, $2, $3, $4, $5)",
-      [
-        testBeer.name,
-        testBeer.abv,
-        testBeer.ibu,
-        testBeer.category_id,
-        testBeer.brewery_id,
-      ]
-    );
   });
 
   it("should return a result greater than 0", async () => {
